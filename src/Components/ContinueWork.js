05d3b055437fc391a4778db51e6a1bf5388f790f
imytdotio <imytdotio@gmail.com>
2023-01-22 20:53:05 +0800

    notes

--- src/Components/ContinueWork.js
+++ src/Components/ContinueWork.js
@@ -10,10 +10,10 @@ export const ContinueWork = (props) => {
   return (
     <div className="p-8 md:w-3/5 w-full md:m-auto">
       <Link to="/create">
+        <h3 className="mb-2 text-left">
+          Continue Writing <strong>Ways to be productive</strong>...
+        </h3>
         <div className="w-full md:flex-row border rounded-md p-4 text-left">
-          <h3 className="mb-2">
-            Continue Writing <strong>Ways to be productive</strong>...
-          </h3>
           <p className="text-gray-700">
             To do this, make a to-do list for the day and prioritize the items
             on it based on their level of importance. You can also use a
--- src/Page/NoteBoard.js
+++ src/Page/NoteBoard.js
@@ -5,17 +5,75 @@ import React from "react";
  * @function NoteBoard
  **/
 
+const NoteBlock = (props) => {
+  return (
+    <div className={`px-4 py-2 pt-4 rounded-xl text-left mb-4 ${props.bg}`}>
+      <h3 className="font-bold mb-2">{props.title}</h3>
+      {props.children}
+    </div>
+  );
+};
+
 export const NoteBoard = (props) => {
   return (
     <div className="flex md:flex-row flex-col w-fit mx-auto">
-      <div className="bg-orange-100 p-4 md:w-72 w-full">
-        <div className="bg-orange-200 p-4"></div>
+      <div className=" p-4 md:w-72 w-full flex-col">
+        <NoteBlock title="Lorem Ipsum" bg="bg-green-100">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </NoteBlock>
+        <NoteBlock title="Lorem Ipsum" bg="bg-blue-100">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </NoteBlock>
+        <NoteBlock title="Lorem Ipsum" bg="bg-green-100">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </NoteBlock>
       </div>
-      <div className="bg-orange-100 p-4 md:w-72 w-full">
-        <div className="bg-orange-200 p-4"></div>
+      <div className="  p-4 md:w-72 w-full flex-col">
+        <NoteBlock title="Lorem Ipsum" bg="bg-orange-100">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </NoteBlock>
+        <NoteBlock title="Lorem Ipsum" bg="bg-red-100">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+        </NoteBlock>
       </div>
-      <div className="bg-orange-100 p-4 md:w-72 w-full">
-        <div className="bg-orange-200 p-4"></div>
+      <div className=" p-4 md:w-72 w-full flex-col">
+        <NoteBlock title="Lorem Ipsum" bg="bg-purple-100">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+        </NoteBlock>{" "}
+        <NoteBlock title="Lorem Ipsum" bg="bg-green-100">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </NoteBlock>
+        <NoteBlock title="Lorem Ipsum" bg="bg-blue-100">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </NoteBlock>
       </div>
     </div>
   );
